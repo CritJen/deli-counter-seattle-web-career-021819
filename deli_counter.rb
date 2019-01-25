a4f3c9e1dcb8602a8693323fdee2b68d9d04ace6
Jennifer Clark <jen.boyd.clark@gmail.com>
2019-01-25 22:56:43 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,3 +17,10 @@ def take_a_number(katz_deli, new_person)
   katz_deli.push(new_person)
   puts "Welcome, #{new_person}. You are number #{katz_deli.length} in line."
 end
+
+def now_serving(katz_deli)
+  if katz_deli.any? == true
+    next_up = katz_deli.shift
+    puts "Currently serving #{next_up}."
+  end
+end
