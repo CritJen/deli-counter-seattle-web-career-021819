22e9fb02f0981a5692b7cfb979aac4a2dd18b8fa
Jennifer Clark <jen.boyd.clark@gmail.com>
2019-01-25 22:48:11 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ katz_deli = []
 def line(katz_deli)
   if katz_deli.any? == true
     deli_line = []
-    katz_deli.each_with_index do |name|
+    katz_deli.each_with_index do |name, index|
       deli_line.push("#{index}. #{name}")
     end
     puts deli_line.join(", ")
